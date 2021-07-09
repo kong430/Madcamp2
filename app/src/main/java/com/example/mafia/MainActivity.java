8114384b7419b52e64745d3ea9d8187cb77eba36
kong430 <alslfkd0430@gmail.com>
2021-07-09 16:01:15 +0900

    login version1

--- app/src/main/java/com/example/mafia/MainActivity.java
+++ app/src/main/java/com/example/mafia/MainActivity.java
@@ -3,12 +3,109 @@ package com.example.mafia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.kakao.sdk.auth.LoginClient;
+import com.kakao.sdk.auth.model.OAuthToken;
+import com.kakao.sdk.user.UserApi;
+import com.kakao.sdk.user.UserApiClient;
+import com.kakao.sdk.user.model.User;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
+import kotlin.jvm.functions.Function2;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private View loginButton, logoutButton;
+    private TextView nickName;
+    private ImageView profileImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.login);
+        logoutButton = findViewById(R.id.logout);
+        nickName = findViewById(R.id.nickname);
+        profileImage = findViewById(R.id.profile);
+
+        //결과에 대한 처리. 로그인 버튼/프로필 사진 보임 or 숨김
+        Function2<OAuthToken, Throwable, Unit> callback = new  Function2<OAuthToken, Throwable, Unit>() {
+            @Override
+            public Unit invoke(OAuthToken oAuthToken, Throwable throwable) {
+                if (oAuthToken != null)  {
+                    return null;
+                }
+                if (throwable != null){
+
+                }
+                updateKakaoLoginUi();
+                return null;
+            }
+        };
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v) {
+               if (LoginClient.getInstance().isKakaoTalkLoginAvailable(MainActivity.this)){
+                   LoginClient.getInstance().loginWithKakaoAccount(MainActivity.this, callback);
+               }
+               else{
+                   //웹페이지
+                    LoginClient.getInstance().loginWithKakaoAccount(MainActivity.this, callback);
+               }
+           }
+       });
+
+        logoutButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                UserApiClient.getInstance().logout(new Function1<Throwable, Unit>() {
+                    @Override
+                    public Unit invoke(Throwable throwable) {
+                        updateKakaoLoginUi();
+                        return null;
+                    }
+                });
+            }
+        });
+
+        updateKakaoLoginUi();
+    }
+
+    private void updateKakaoLoginUi() {
+        UserApiClient.getInstance().me(new Function2<User, Throwable, Unit>() {
+            @Override
+            public Unit invoke(User user, Throwable throwable) {
+                if (user != null) {
+                    Log.i(TAG, "invoke: id=" + user.getId());
+                    Log.i(TAG, "invoke: email=" + user.getKakaoAccount().getEmail());
+                    Log.i(TAG, "invoke: nickname=" + user.getKakaoAccount().getProfile().getNickname());
+                    Log.i(TAG, "invoke: gender=" + user.getKakaoAccount().getGender());
+                    Log.i(TAG, "invoke: age=" + user.getKakaoAccount().getAgeRange());
+
+
+                    nickName.setText(user.getKakaoAccount().getProfile().getNickname());
+                    Glide.with(profileImage).load(user.getKakaoAccount().getProfile().getThumbnailImageUrl()).circleCrop().into(profileImage);
+
+                    loginButton.setVisibility(View.GONE);
+                    logoutButton.setVisibility(View.VISIBLE);
+                } else {
+                    nickName.setText(null);
+                    profileImage.setImageBitmap(null);
+                    loginButton.setVisibility(View.VISIBLE);
+                    logoutButton.setVisibility(View.GONE);
+                }
+                return null;
+            }
+        });
     }
 }
