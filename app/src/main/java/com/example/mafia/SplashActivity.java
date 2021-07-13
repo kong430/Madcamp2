ee2dc84518918e410ccef32830c62c783b320031
kong430 <alslfkd0430@gmail.com>
2021-07-13 21:53:45 +0900

    add animation

--- app/src/main/java/com/example/mafia/SplashActivity.java
+++ app/src/main/java/com/example/mafia/SplashActivity.java
@@ -3,17 +3,42 @@ package com.example.mafia;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
+    Animation anim;
+    private TextView textView3;
+    private TextView textView4;
+    private TextView textView5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        textView3 = findViewById(R.id.textView3);
+        textView4 = findViewById(R.id.textView4);
+        textView5 = findViewById(R.id.textView5);
+
+        anim = new AlphaAnimation(0.0f,1.0f);
+        anim.setDuration(100);
+        anim.setStartOffset(20);
+        anim.setRepeatMode(Animation.REVERSE);
+        anim.setRepeatCount(Animation.INFINITE);
+
+        textView3.startAnimation(anim);
+        textView4.startAnimation(anim);
+        textView5.startAnimation(anim);
+
+
         Handler hd = new Handler();
         hd.postDelayed(new splashhandler(), 2000); // 1초 후에 hd handler 실행  3000ms = 3초
+
     }
     private class splashhandler implements Runnable{
         public void run(){
